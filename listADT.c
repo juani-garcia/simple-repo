aebb5a3bc647f4b46819079545e316c364ba1287
juanigarcia <juanigarcia@itba.edu.ar>
2022-06-29 18:57:23 -0300

    Add function isEmprty

---
+++ listADT.c
@@ -0,0 +1,14 @@
+//
+//
+// Lista de enteros ordenada y sin repetidos
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include "listADT.h"
+
+
+int isEmpty(TList l) {
+    return l == NULL;
+}
